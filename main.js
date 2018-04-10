fa818750ffdbf71b152f7f567d0d6a3d4d614330
Ali Rahman <amrahman@mit.edu>
2018-04-10 01:16:57 -0400

    added initial stage drawing screen

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let dom = {}; // Holds DOM elements that don’t change, to avoid repeatedly querying the DOM
+let stageDrawing = new StageDrawing(dom); // load stageDrawing code
+let stageView = new StageView(dom); // load stageView code
 
 // Attaching events on document because then we can do it without waiting for
 // the DOM to be ready (i.e. before DOMContentLoaded fires)
@@ -6,7 +8,7 @@ Util.events(document, {
     // Final initalization entry point: the Javascript code inside this block
     // runs at the end of start-up when the DOM is ready
     "DOMContentLoaded": () => {
-        // Element refs
-        dom.stageView = Util.one("#stageView");
+        stageDrawing.domContentLoaded();
+        stageView.domContentLoaded();
     }
 });
--- stageview.js
+++ stageview.js
@@ -1,3 +1,16 @@
 class StageView extends EventTarget {
+    constructor(dom){
+        super();
+        this.dom = dom;
+    }
 
+    domContentLoaded(){
+        dom.stageView = Util.one("#stageView");
+        dom.addDancer = Util.one("#addDancer");
+        dom.removeDancer = Util.one("#removeDancer");
+
+        dom.addDancer.addEventListener("click", () => {
+            console.log("ADD");
+        });
+    }
 }
