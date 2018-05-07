74a25ee710e81f29948bdd9b5911445f9046939b
Rodrigo Carrillo-Sanchez <rcarrill@mit.edu>
2018-05-07 15:12:40 -0400

    groups behavior fixed, still need to incorporate into stageview state

--- groupsview.js
+++ groupsview.js
@@ -22,13 +22,9 @@ class GroupsView {
     }
 
     getColorOfGroup(id){
-        if (id != -1) {
+        if (this.actives.has(id)) {
             let activeGroup = document.getElementById("group" + id);
-            if (this.actives.get(id)){
-                return activeGroup.style.backgroundColor;
-            } else {
-                return "rgb(60, 60, 60)"
-            }
+            return activeGroup.style.backgroundColor;
         } else {
             return "rgb(60, 60, 60)";
         }
@@ -44,31 +40,14 @@ class GroupsView {
 
     addGroup() {
         this.curr = this.curr + 1;
-        let newGroup = document.createElement("div");
+        let newGroup = document.createElement("button");
         newGroup.setAttribute("id", "group" + this.curr);
         let ind = this.curr % this.colors.size;
         newGroup.style.background = this.colors.get(ind);
         let newGroupDelete = document.createElement("button");
-        let newGroupActive = document.createElement("button");
-        let newGroupInactive = document.createElement("button");
-        newGroupActive.setAttribute("id", "activate"+this.curr);
-        newGroupInactive.setAttribute("id", "deactivate"+this.curr);
-        //let child1 = document.createElement("input");
-        // child1.setAttribute("type", "checkbox");
-        // let child2 = document.createElement("span");
-        // child2.classList.add("slider");
-        // newGroupActive.appendChild(child1);
-        // newGroupActive.appendChild(child2);
         newGroupDelete.innerHTML = "🗑️";
-        newGroupActive.innerHTML = "➕";
-        newGroupInactive.innerHTML = "➖";
         newGroupDelete.setAttribute("onClick", "groupsView.deleteGroup(" + this.curr + ");");
-        newGroupActive.setAttribute("onClick", "groupsView.activateGroup(" + this.curr + ");");
-        newGroupInactive.setAttribute("onClick", "groupsView.deactivateGroup(" + this.curr + ");");
-        newGroupActive.style.borderWidth = "1px";
-        newGroupActive.style.borderColor = "transparent";
-        newGroupActive.classList.add("addButton");
-        newGroupInactive.classList.add("addButton");
+        newGroup.setAttribute("onClick", "groupsView.activateGroup(" + this.curr + ");");
         newGroupDelete.classList.add("deleteGroup");
         newGroupDelete.style.alignSelf = "right";
         newGroup.innerHTML = "Group " + this.curr;
@@ -77,15 +56,13 @@ class GroupsView {
         child0.style.width = "100%";
         let separator = document.createElement("div");
         separator.style.width = "20%";
-        child0.appendChild(newGroupActive);
-        child0.appendChild(newGroupInactive);
         child0.appendChild(separator);
         child0.appendChild(newGroupDelete);
         newGroup.appendChild(child0);
         dom.groupsDiv.appendChild(newGroup);
         this.groups.set(this.curr, newGroup);
         this.actives.set(this.curr, false);
-        newGroupActive.focus();
+        newGroup.focus();
     }
 
     deleteGroup(ind) {
@@ -96,52 +73,32 @@ class GroupsView {
     }
 
     activateGroup(ind) {
-        var activeGroupI = eval(ind);
-        let activeGroup = document.getElementById("group" + activeGroupI);
-        let activeButton = document.getElementById("activate" + ind);
-        let inactiveButton = document.getElementById("deactivate"+ind);
-        if (this.actives.get(ind) === false) {
-            var i;
-            for(i = 0; i < this.curr+1; i++){
-                if(this.actives.get(i) === true) {
-                    this.activateGroup(i);
-                }
-            }            
-            this.actives.set(ind, true);
-            activeGroup.style.boxShadow = "0px 0px 20px rgba(0, 89, 255, 0.7) inset";
-            activeGroup.style.color = "white";
-            activeGroup.style.fontWeight = "bold";
-            activeGroup.style.borderColor = "aqua";
-            //activeButton.style.background = "limegreen";
-            activeButton.style.borderWidth = "5px";
-            activeButton.style.borderColor = "white";
-            activeButton.style.background = "navy";
-            inactiveButton.style.background = "transparent";
-            this.active = ind;
-        } else {
-            this.actives.set(ind, false);
-            activeGroup.style.boxShadow = "none";
-            activeGroup.style.color = "black";
-            activeGroup.style.fontWeight = "normal";
-            activeGroup.style.borderColor = "black";
-            activeGroup.style.borderWidth = "1px";
-            activeButton.style.background = "transparent";
-            this.active = -1;
+        if (this.actives.has(eval(ind))){
+            var activeGroupI = eval(ind);
+            let activeGroup = document.getElementById("group" + activeGroupI);
+            if (this.actives.get(ind) === false) {
+                var i;
+                for(i = 0; i < this.curr+1; i++){
+                    if(this.actives.get(i) === true) {
+                        this.activateGroup(i);
+                    }
+                }            
+                this.actives.set(ind, true);
+                activeGroup.style.boxShadow = "0px 0px 20px rgba(0, 89, 255, 0.7) inset";
+                activeGroup.style.color = "white";
+                activeGroup.style.fontWeight = "bold";
+                activeGroup.style.borderColor = "aqua";
+                this.active = ind;
+            } else {
+                this.actives.set(ind, false);
+                activeGroup.style.boxShadow = "none";
+                activeGroup.style.color = "black";
+                activeGroup.style.fontWeight = "normal";
+                activeGroup.style.borderColor = "gray";
+                this.active = -1;
+            }
         }
+        // else the group has been removed and nothing should be done
     }
-    deactivateGroup(ind){
-        // this is when a group's - button is pressed
-        // so technically focus is still on it but we can't add dancers anymore
-        var activeGroupI = eval(ind);
-        let activeGroup = document.getElementById("group" + activeGroupI);
-        let activeButton = document.getElementById("activate" + ind);
-        let inactiveButton = document.getElementById("deactivate"+ind);
-        this.actives.set(ind, false);
-        activeGroup.style.boxShadow = "0px 0px 20px rgba(0, 89, 255, 0.7) inset";
-        activeGroup.style.color = "white";
-        activeGroup.style.fontWeight = "bold";
-        activeGroup.style.borderColor = "aqua";
-        activeButton.style.background = "transparent";    
-        inactiveButton.style.background = "navy";
-    }
+
 }
